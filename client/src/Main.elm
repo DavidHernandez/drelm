deb71d625c1cae13ee4ab451ee83e5941414fcc9
David Hernandez <davidhernandezruiz@gmail.com>
2018-04-17 23:40:23 +0200

    Introducing the Elm archtecture.

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -1,7 +1,53 @@
 module Main exposing (main)
 
-import Html exposing (text)
+import Html exposing (Html, div, text)
+import List
+
+
+type alias Model =
+    { posts : List Post
+    , activePage : Page
+    }
+
+
+type Page
+    = BlogList
+    | Blog
+
+
+type alias Post =
+    String
+
+
+type Msg
+    = NoOp
+
+
+model : Model
+model =
+    { posts = [ "First blog", "Second blog" ]
+    , activePage = BlogList
+    }
+
+
+update : Msg -> Model -> Model
+update msg model =
+    model
+
+
+view : Model -> Html Msg
+view model =
+    div
+        []
+        (List.map viewPost model.posts)
+
+
+viewPost : Post -> Html Msg
+viewPost post =
+    div
+        []
+        [ text post ]
 
 
 main =
-    text "Hello world"
+    view model
