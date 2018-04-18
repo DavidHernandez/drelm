9ec485153deb4977784b7e95d1d52ce8fee903a6
David Hernandez <davidhernandezruiz@gmail.com>
2018-04-19 00:51:09 +0200

    Adds decoders plus webdata data structure.

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -3,13 +3,26 @@ module Main exposing (main)
 import Dict exposing (Dict)
 import Html exposing (Html, a, div, h2, text, beginnerProgram)
 import Html.Events exposing (onClick)
+import Json.Decode exposing (Decoder, int, string, dict)
+import Json.Decode.Pipeline exposing (decode, required)
 import List
 
 
 type alias Model =
-    { posts : Dict PostId Post
+    { posts : WebData PostList
     , activePage : Page
     }
+
+
+type WebData data
+    = NotAsked
+    | Loading
+    | Error
+    | Success data
+
+
+type alias PostList =
+    Dict PostId Post
 
 
 type Page
@@ -18,7 +31,7 @@ type Page
 
 
 type alias PostId =
-    Int
+    String
 
 
 type alias Post =
@@ -31,14 +44,14 @@ type alias Post =
 
 model : Model
 model =
-    { posts = Dict.fromList [ ( 1, firstPost ), ( 2, secondPost ) ]
+    { posts = NotAsked
     , activePage = BlogList
     }
 
 
 firstPost : Post
 firstPost =
-    { id = 1
+    { id = "1"
     , title = "First blog"
     , body = "This is the body of the first blog post"
     , created = "2018-04-18 19:00"
@@ -47,7 +60,7 @@ firstPost =
 
 secondPost : Post
 secondPost =
-    { id = 2
+    { id = "2"
     , title = "Second blog"
     , body = "This is the body of the another blog post"
     , created = "2018-05-18 20:00"
@@ -67,36 +80,47 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    case model.activePage of
-        BlogList ->
-            viewBlogList model.posts
+    case model.posts of
+        NotAsked ->
+            div [] [ text "Loading..." ]
 
-        Blog postId ->
-            let
-                post =
-                    Dict.get postId model.posts
-            in
-                case post of
-                    Just aPost ->
-                        div
-                            []
-                            [ h2 [] [ text aPost.title ]
-                            , div [] [ text aPost.created ]
-                            , div [] [ text aPost.body ]
-                            , a [ onClick <| NavigateTo BlogList ] [ text "Go back" ]
-                            ]
+        Loading ->
+            div [] [ text "Loading..." ]
 
-                    Nothing ->
-                        div
-                            [ onClick <| NavigateTo BlogList ]
-                            [ text "Blog post not found" ]
+        Success posts ->
+            case model.activePage of
+                BlogList ->
+                    viewBlogList posts
+
+                Blog postId ->
+                    let
+                        post =
+                            Dict.get postId posts
+                    in
+                        case post of
+                            Just aPost ->
+                                div
+                                    []
+                                    [ h2 [] [ text aPost.title ]
+                                    , div [] [ text aPost.created ]
+                                    , div [] [ text aPost.body ]
+                                    , a [ onClick <| NavigateTo BlogList ] [ text "Go back" ]
+                                    ]
+
+                            Nothing ->
+                                div
+                                    [ onClick <| NavigateTo BlogList ]
+                                    [ text "Blog post not found" ]
+
+        Error ->
+            div [] [ text "Error loading the data" ]
 
 
-viewBlogList : Dict PostId Post -> Html Msg
+viewBlogList : PostList -> Html Msg
 viewBlogList posts =
     div
         []
-        (Dict.map viewPostTeaser model.posts |> Dict.values)
+        (Dict.map viewPostTeaser posts |> Dict.values)
 
 
 viewPostTeaser : PostId -> Post -> Html Msg
@@ -104,6 +128,20 @@ viewPostTeaser postId post =
     div
         [ onClick <| NavigateTo <| Blog postId ]
         [ text post.title ]
+
+
+postListDecoder : Decoder PostList
+postListDecoder =
+    dict postDecoder
+
+
+postDecoder : Decoder Post
+postDecoder =
+    decode Post
+        |> required "id" string
+        |> required "title" string
+        |> required "body" string
+        |> required "created" string
 
 
 main : Program Never Model Msg
